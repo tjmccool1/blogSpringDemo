a69d5f146dc31ba4c1297c7f272238f561e71f9b
TMcCool <tjmccool1@icloud.com>
2018-04-16 14:56:11 -0500

    added Hello World Controller

---
+++ src/main/java/com/codeup/demo/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.codeup.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World, I am alive!";
+    }
+}
