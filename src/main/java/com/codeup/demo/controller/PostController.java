acbd43048b4fc4f6078f1ebdf486b4914a8756fb
TMcCool <tjmccool1@icloud.com>
2018-04-18 09:26:53 -0500

    stiil nee to finish

--- src/main/java/com/codeup/demo/controller/PostController.java
+++ src/main/java/com/codeup/demo/controller/PostController.java
@@ -1,32 +1,50 @@
 package com.codeup.demo.controller;
 
 
+import com.codeup.demo.model.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
-    @GetMapping("/post")
-    @ResponseBody
-    public String index(){
+    @GetMapping("/posts")
+
+    public String index(Model model){
+        List<Post> posts = new ArrayList<>();
+
+        posts.add(new Post("This is title 1", "This is the 1st body"));
+        posts.add(new Post("This is title 2", "This is the 2nd body"));
+        posts.add(new Post("This is title 3", "This is the 3rd body"));
+        posts.add(new Post("This is title 4", "This is the 4th body"));
+
+        model.addAttribute("posts", posts);
+
         return "post index page";
     }
+
     @GetMapping("/post/{id}")
-    @ResponseBody
-    public String show(@PathVariable long id){
-        return "post" + id;
+
+    public String show(@PathVariable long id, Model model){
+        Post post = new Post("Test post", "Test body describtion");
+
+        model.addAttribute("post", post);
+        return "post/show";
     }
 
     @GetMapping("/post/create")
-    @ResponseBody
+
     public String ShowCreateForm(){
         System.out.println("view form");
         return "view form";
     }
 
     @PostMapping("/post/create")
-    @ResponseBody
+
     public String createPost(@RequestParam String name){
         System.out.println("submitted form");
         return "test is complete, " + name;
