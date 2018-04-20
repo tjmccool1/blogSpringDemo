4810d55114c943d2d9db271291bbe4e210e5c4b5
TMcCool <tjmccool1@icloud.com>
2018-04-20 09:15:05 -0500

    finished up some stuff

---
+++ src/main/java/com/codeup/demo/model/User.java
@@ -0,0 +1,26 @@
+package com.codeup.demo.model;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
