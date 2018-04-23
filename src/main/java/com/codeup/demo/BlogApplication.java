2a163b27f023b4c1449179a6fba0a0d4caef799d
TMcCool <tjmccool1@icloud.com>
2018-04-23 14:51:07 -0500

    deployment setup

--- src/main/java/com/codeup/demo/BlogApplication.java
+++ src/main/java/com/codeup/demo/BlogApplication.java
@@ -2,11 +2,18 @@ package com.codeup.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BlogApplication.class);
+	}
+
 }
