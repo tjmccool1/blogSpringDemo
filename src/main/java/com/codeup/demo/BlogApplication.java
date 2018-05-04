48947e90d8e37b75945b2aa3c49e29da90952e21
TMcCool <tjmccool1@icloud.com>
2018-05-04 03:30:30 -0500

    update

--- src/main/java/com/codeup/demo/BlogApplication.java
+++ src/main/java/com/codeup/demo/BlogApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class BlogApplication extends SpringBootServletInitializer {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
